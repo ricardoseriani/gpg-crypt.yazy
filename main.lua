61bdbe9461f872e37de2f2c20546b20a44b52e33
Ricardo Seriani <ricardo.seriani@gmail.com>
2025-07-03 16:52:26 -0300

    Extract guard conditions to simplify code

--- main.lua
+++ main.lua
@@ -28,20 +28,25 @@ return {
             }
 
             -- Check if The user has confirmed both inputs
-            if crypt_key_event == 1 and confirm_crypt_key_event == 1 then
-                -- Check if both key is equals
-                if crypt_key == confirm_crypt_key then
-                    for _, v in pairs(selected_or_hovered()) do
-                        if fs.cha(v).is_dir then
-                            -- TODO: Check a way to use <() inside os.execute
-                            local zipped = tostring(v)..".tar"
-                            os.execute("tar -cf " .. zipped .. " " .. tostring(v))
-                            os.execute("gpg --quiet --symmetric --output " .. zipped .. ".gpg --batch --passphrase " .. crypt_key .. " " .. zipped)
-                            os.execute("rm " .. zipped)
-                        else
-                            os.execute("gpg --quiet --symmetric --output " .. tostring(v) .. ".gpg --batch --passphrase " .. crypt_key .. " " .. tostring(v))
-                        end
-                    end
+            if crypt_key_event ~= 1 or confirm_crypt_key_event ~= 1 then
+                return
+            end
+
+            -- Check if both key is equals
+            if crypt_key ~= confirm_crypt_key then
+                return
+            end
+
+            -- Encrypt files/directories
+            for _, v in pairs(selected_or_hovered()) do
+                if fs.cha(v).is_dir then
+                    -- TODO: Check a way to use <() inside os.execute
+                    local zipped = tostring(v)..".tar"
+                    os.execute("tar -cf " .. zipped .. " " .. tostring(v))
+                    os.execute("gpg --quiet --symmetric --output " .. zipped .. ".gpg --batch --passphrase " .. crypt_key .. " " .. zipped)
+                    os.execute("rm " .. zipped)
+                else
+                    os.execute("gpg --quiet --symmetric --output " .. tostring(v) .. ".gpg --batch --passphrase " .. crypt_key .. " " .. tostring(v))
                 end
             end
         end
@@ -53,10 +58,13 @@ return {
             }
 
             -- Check if The user has confirmed input
-            if crypt_key_event == 1 then
-                for _, v in pairs(selected_or_hovered()) do
-                    os.execute("gpg --decrypt --output " .. tostring(v):gsub(".gpg$","") .. " --batch --passphrase " .. crypt_key .. " " .. tostring(v))
-                end
+            if crypt_key_event ~= 1 then
+                return
+            end
+
+            -- Decrypt files/directories
+            for _, v in pairs(selected_or_hovered()) do
+                os.execute("gpg --decrypt --output " .. tostring(v):gsub(".gpg$","") .. " --batch --passphrase " .. crypt_key .. " " .. tostring(v))
             end
         end
     end,
